4f41a6f30a118c1922df900180cf9ca70ef45944
paulo <pclacerda@gmail.com>
2023-06-26 12:49:20 -0300

    initial

---
+++ README.md
@@ -0,0 +1,96 @@
+# gpt on your data frontend
+
+## Build process
+
+Everytime you change frontend code you need to build it before a new deployment:
+
+```
+cd frontend
+npm install
+npm run build
+```
+
+## Deploy to Azure
+
+**1) Deployment pre-requirements**
+
+- Deploy the Chatbot orchestrator in a Function App.
+
+- Provision an Azure Speech Service for the speech to text and text to speech part.
+
+**2) Create Web App**
+
+Create a Web App (App Service) with Python 3.10 runtime.
+
+**3) Set Startup Command**
+
+In Azure Portal > Web App > Configuration > General Settings > Startup Command add:
+
+```python ./app.py```
+
+**4) Set Application Settings**
+
+In Azure Portal > Web App > Configuration > Application Settings:
+
+Add the application settings listed on [.env.template](.env.template), adjusting values accordingly to your environment.
+
+**5) Deploy to Azure** (tthis step can be repeated whenever you make changes to the front)
+
+In VSCode with [Azure Web App Extension](https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-azureappservice) go to the *Azure* Window, reveal your Web App in the resource explorer, right-click it then select *Deploy to Web App*.
+
+
+## Frontend customizations
+
+**1) Blob storage path**
+
+Update the blob storage path in the getCitationFilePath function to point to where your data is.
+
+file: ```frontend/src/api/api.ts```
+
+Example: storage account ```177960botistorage``` and storage container ```boti```
+```
+export function getCitationFilePath(citation: string): string {
+    return `https://177960botistorage.blob.core.windows.net/boti/${citation}`;
+}
+```
+
+**2) Title**
+
+Update page's title
+
+file: ```frontend/src/pages/layout/Layout.tsx```
+
+```
+<h4 className={styles.headerRightText}>Chat Boti</h4>
+```
+
+file: ```frontend/src/pages/layout/index.html```
+
+```
+<title>Chat Boti | Demo</title>
+```
+
+**3) Logo**
+
+Update frontend logo
+
+file: ```frontend/src/pages/layout/Layout.tsx```
+
+Example:
+```
+<Link to="/" className={styles.headerTitleContainer}>
+    <img height="80px" src="https://buenosaires.gob.ar/sites/default/files/media/image/2022/06/16/330682881a2d085e5f614b4b0b3b24aacb37ffce.png"></img>
+    <h3 className={styles.headerTitle}></h3>
+</Link>
+```
+
+**4) Speech Synthesis**
+
+To enable speech synthesis change speechSynthesisEnabled variable to true.
+
+file: ```frontend/src/pages/chat/Chat.tsx```
+
+```
+const speechSynthesisEnabled = true;
+```
+
---
+++ frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,53 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
+
+import github from "../../assets/github.svg";
+
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <img height="80px" src="https://buenosaires.gob.ar/sites/default/files/media/image/2022/06/16/330682881a2d085e5f614b4b0b3b24aacb37ffce.png"></img>
+                        <h3 className={styles.headerTitle}></h3>
+                    </Link>
+                    <nav>
+                        {/*
+                        <ul className={styles.headerNavList}>
+                            <li>
+                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Chat
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Ask a question
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
+                                    <img
+                                        src={github}
+                                        alt="Github logo"
+                                        aria-label="Link to github repository"
+                                        width="20px"
+                                        height="20px"
+                                        className={styles.githubLogo}
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+    */}
+                    </nav>
+                    <h4 className={styles.headerRightText}>Chat Boti</h4>
+                </div>
+            </header>
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
